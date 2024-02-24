ca7bbd55f3ee67b2819f8bb44e8c65dc05bb55b2
CY12-bit <coliny911@gmail.com>
2024-02-23 17:03:21 -0800

    Created more helper functions in cifar10_import.py. Did more work in kNN.ipynb

--- Notebooks/cifar10_import.py
+++ Notebooks/cifar10_import.py
@@ -9,6 +9,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
 def importCifar10(type = "split"):
     """
     Imports dataset and returns dataset based on type.
@@ -21,21 +24,50 @@ def importCifar10(type = "split"):
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
     if type == "split": 
-        return (x_train, y_train), (x_test, y_test)
+        return (x_train, np.array([y[0] for y in y_train])), (x_test, np.array([y[0] for y in y_test]))
     elif type == "combined":
         x = np.vstack((x_train, x_test))
         y = np.vstack((y_train,y_test))
-        return (x,y)
+        return (x,np.array([y[0] for y in y]).T)
     else: 
         return None
-    
 
-def grayScale(x_images:np.array) -> np.array:
-    grayScaled = list()
-    for x in x_images:
-        gray = cv2.cvtColor(x, cv2.COLOR_RGB2GRAY)
-        grayScaled.append(gray)
-    
-    return np.array(grayScaled)
+def prepareX(x, x_test = None, gray_scale = False, scaled = True):
+    """
+    Function flattens x to be a 2d array and conditionally standardizes it.
+    """
+    col_size = 3072 if gray_scale == False else 1024
 
+    x_scaled = x.flatten().reshape(len(x),col_size)
+    if scaled == True:
+        scalar = StandardScaler()
+        x_scaled = scalar.fit_transform(x_scaled)
+        return (x_scaled, scalar.transform(x_test.flatten().reshape(len(x_test),col_size))) if type(x_test) != type(None) else x_scaled
+    else:
+        return (x_scaled, x_test.flatten().reshape(len(x_test),col_size)) if type(x_test) != type(None) else x_scaled
 
+def grayScaleData(*args):
+    """
+    Function turns a colored image array into a gray-scaled image array
+    """
+    return (
+        np.array([cv2.cvtColor(x, cv2.COLOR_RGB2GRAY) for x in x_images]) \
+        for x_images in args
+    )
+
+def printFirst64(x,y):
+    plt.figure(figsize=(15,15))
+    plt.rcParams['image.cmap'] = 'gray'
+    # Loop over the first 64 images
+    for i in range(64):
+        # Create a subplot for each image
+        
+        plt.subplot(8, 8, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+
+        c = plt.imshow(x[i])
+
+        # Set the label as the title
+        plt.title(class_names[y[i]], fontsize=12)
